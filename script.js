cc96f7a9875d52c64c73eff82ee5b6bf864955f4
Nelson Caña <nelsoncanav2000@gmail.com>
2026-06-21 01:38:06 -0400

    responsive para móvil

--- script.js
+++ script.js
@@ -211,3 +211,19 @@ function enviarFormulario(e) {
   btn.textContent = '✅ Mensaje enviado!';
   setTimeout(() => { btn.textContent = 'Enviar mensaje →'; e.target.reset(); }, 3000);
 }
+
+// MENÚ HAMBURGUESA
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navLinks.classList.toggle('active');
+});
+
+navLinks.querySelectorAll('a').forEach(link => {
+  link.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navLinks.classList.remove('active');
+  });
+});
